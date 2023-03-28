656e448835151f43ee804edb8b03bf7217ecab42
raj-j-shah <rajcoder19@gmail.com>
2023-03-28 14:02:00 +0530

    changes

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const fire = require('./Db/mongoose');
 app.use(express.json());
 app.use((req,res,next)=>{
     if(mongoose.connection.readyState==2||mongoose.connection.readyState==0){
-    fire();
+    fire()
     res.status(400).send({"message":"db is down"});
     }
     else
